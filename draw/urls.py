f31fb39651c8eb674e8dc611338c6377bf384557
Noah Schwartz <n0112358@berkeley.edu>
2021-07-27 02:56:24 -0700

    Done?

--- draw/urls.py
+++ draw/urls.py
@@ -8,6 +8,15 @@ urlpatterns = [
     path('<str:room_name>/', views.room, name='room'),
     path('genres_1', views.genres_1, name='genres_1'),
     path('final', views.final, name='final'),
+    path('album', views.album, name='album'),
+    path('album_variant_two', views.album_variant_two, name='album_variant_two'),
+    path('end_screen', views.end_screen, name='end_screen'),
+    path('indie_artists', views.indie_artists, name='indie_artists'),
+    path('pop_artists', views.pop_artists, name='pop_artists'),
+    path('rap_artists', views.rap_artists, name='rap_artists'),
+    path('songs', views.songs, name='songs'),
+    path('version', views.version, name='version'),
+    path('final', views.final, name='final'),
     path('display', views.display, name='display'),
 
 ]
--- draw/views.py
+++ draw/views.py
@@ -4,6 +4,30 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'draw/index.html')
 
+def album(request):
+    return render(request, 'draw/album.html')
+
+def album_variant_two(request):
+    return render(request, 'draw/album_variant_two.html')
+
+def end_screen(request):
+    return render(request, 'draw/end_screen.html')
+
+def indie_artists(request):
+    return render(request, 'draw/indie_artists.html')
+
+def pop_artists(request):
+    return render(request, 'draw/pop_artists.html')
+
+def rap_artists(request):
+    return render(request, 'draw/rap_artists.html')
+
+def songs(request):
+    return render(request, 'draw/songs.html')
+
+def version(request):
+    return render(request, 'draw/version.html')
+
 def genres_1(request):
     return render(request, 'draw/genres_1.html')
 
